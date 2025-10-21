9daa3d08a780fa8cd154807fbfb15edc8658ecd8
Villy Rosa <villyrosa69@gmail.com>
2025-10-21 19:39:30 -0300

    feat: generate overview page and setup auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { Login } from '@pages/login/login';
 import { Register } from '@pages/register/register';
+import { authGuard } from './guards/auth-guard';
+import { Overview } from '@pages/overview/overview';
 
 export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
+      { path: 'overview', component: Overview },
+    ]
+  },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 ];
